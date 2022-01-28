6afd268b12f8f6c3d3eb1e300df79ee8f28f8734
satriya.wicaksana888 <satriya.wicaksana888@gmail.com>
2022-01-28 20:03:03 +0700

    [FEATURE] Adding basic style for mobile

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,7 +3,15 @@ import './Result.css';
 
 class Result extends Component {
   render() {
-    return <div className='result'></div>;
+    return (
+      <div className='result'>
+        <div className='result-toggle'>nightmode</div>
+        <div>
+          <p className='text-right result-operation'>operation</p>
+          <p className='text-right result-display'>Result</p>
+        </div>
+      </div>
+    );
   }
 }
 
