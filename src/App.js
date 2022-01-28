bea5b4d8f46f775b02bd999d30b7bf139806965c
satriya.wicaksana888 <satriya.wicaksana888@gmail.com>
2022-01-28 22:14:00 +0700

    [FEATURE] enable toggle for night mode

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import React, { Component } from 'react';
 import Calculator from './UI/Calculator';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { nightMode: false };
-  }
   render() {
     return (
-      <div
-        className={'app ' + (this.state.nightMode ? 'app-night' : 'app-day')}>
-        <Calculator />
+      <div className='app'>
+        <Calculator stateData={this.state} />
       </div>
     );
   }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import NightIcon from '../assets/moon-fill.svg';
+import DayIcon from '../assets/sun-fill.svg';
 import './Result.css';
 
 class Result extends Component {
+  constructor() {
+    super();
+    this.state = { nightMode: true };
+  }
+  toggleNightMode = () => {
+    this.setState({
+      nightMode: !this.state.nightMode,
+    });
+  };
   render() {
     return (
       <div className='result'>
-        <div className='result-toggle'>nightmode</div>
+        <div className='result-toggle'>
+          <label htmlFor='toggle-checkbox' className='toggle-control'>
+            <input
+              checked={this.state.nightMode}
+              onChange={this.toggleNightMode}
+              id='toggle-checkbox'
+              type='checkbox'
+              className='toggle-checkbox'
+            />
+            <span
+              className='control'
+              style={{
+                backgroundImage: this.state.nightMode
+                  ? `url(${NightIcon})`
+                  : `url(${DayIcon})`,
+                backgroundPositionX: this.state.nightMode ? '20%' : '80%',
+              }}>
+              {/* <img src={NightIcon} alt='N' /> */}
+            </span>
+          </label>
+        </div>
         <div>
           <p className='text-right result-operation'>operation</p>
           <p className='text-right result-display'>Result</p>
